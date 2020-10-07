386dc117e78229b3bf7528356c6e0a6227d74d34
medbm5 <medbaamrani0@gmail.com>
2020-10-07 19:48:37 +0100

    Updates

---
+++ precache-manifest.73ea2d9e26e24ed4cf91ad282f5ef5a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a87af0f12c1f4295b77cfe2401ffbde0",
+    "url": "/robots/index.html"
+  },
+  {
+    "revision": "2364c741804480acc851",
+    "url": "/robots/static/css/main.e7e00a6c.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/robots/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robots/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2364c741804480acc851",
+    "url": "/robots/static/js/main.0e2c2217.chunk.js"
+  },
+  {
+    "revision": "b23e05c3adb9d44bc70a",
+    "url": "/robots/static/js/runtime-main.9810f4fd.js"
+  }
+]);
